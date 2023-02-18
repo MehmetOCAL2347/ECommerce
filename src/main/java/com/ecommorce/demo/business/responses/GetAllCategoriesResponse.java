91abf0b5583253609a3bf4fdd390cc28a058304a
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-18 11:21:23 +0300

    Business paketi altına responses paketi eklendi ve içerisine classımız oluşturuldu

---
+++ src/main/java/com/ecommorce/demo/business/responses/GetAllCategoriesResponse.java
@@ -0,0 +1,17 @@
+package com.ecommorce.demo.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetAllCategoriesResponse {
+    private int id;
+    private String name;
+    private String imageURL;
+    private String description;
+}
