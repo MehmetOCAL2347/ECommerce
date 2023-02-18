81d8484f0bcd11d1ebfa25cd3c2f04238613cf98
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-18 08:44:19 +0300

    Category Değişkenleri oluşturuldu

--- src/main/java/com/ecommorce/demo/entities/Category.java
+++ src/main/java/com/ecommorce/demo/entities/Category.java
@@ -1,4 +1,31 @@
 package com.ecommorce.demo.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "imageURL")
+    private String imageURL;
+
+    @Column(name = "description")
+    private String description;
+
 }
