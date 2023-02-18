4345db7ded0e708248585f5d6771a05c6d8f6474
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-18 11:55:25 +0300

    Category Manager'a categoryRepository objesi eklendi

--- src/main/java/com/ecommorce/demo/business/concretes/CategoryManager.java
+++ src/main/java/com/ecommorce/demo/business/concretes/CategoryManager.java
@@ -2,14 +2,32 @@ package com.ecommorce.demo.business.concretes;
 
 import com.ecommorce.demo.business.abstracts.CategoryService;
 import com.ecommorce.demo.business.responses.GetAllCategoriesResponse;
+import com.ecommorce.demo.dataAccess.CategoryRepository;
+import com.ecommorce.demo.entities.Category;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryManager implements CategoryService {
 
+    private CategoryRepository categoryRepository;
+
     @Override
     public List<GetAllCategoriesResponse> getAllCategories() {
         // Buraya business kuralları eklenir
-        return null;
+        // Buradaki tip dönüşümü başka türlü yapılabilir araştırmak lazım
+        List<Category> categories = categoryRepository.findAll();
+        List<GetAllCategoriesResponse> brandsResponses = new ArrayList<>();
+
+        for (Category category: categories) {
+            GetAllCategoriesResponse responseItem = new GetAllCategoriesResponse();
+            responseItem.setId(category.getId());
+            responseItem.setName(category.getName());
+            responseItem.setImageURL(category.getImageURL());
+            responseItem.setDescription(category.getDescription());
+            brandsResponses.add(responseItem);
+        }
+
+        return brandsResponses;
     }
 }
